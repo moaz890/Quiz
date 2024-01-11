b4a6bf32c271b81d5f7d59b2ae76823d2b191719
moaz890 <mozagazzer@gmail.com>
2024-01-11 21:14:42 +0200

    Dist

---
+++ dist/src/js/data.js
@@ -0,0 +1,37 @@
+
+
+export default {
+
+    questions: [
+        {
+            id: 1,
+            title: "The instructions that tell the computer what to do and how to do it:",
+            answers: ["Software", "Hardware", "Mouse", "Monitor"],
+            correct: "Software",
+        },
+        {
+            id: 2,
+            title: "The heart (and brain) of the computer is :",
+            answers: ["CPU", "ROM", "RAM", "Hard Drive"],
+            correct: "CPU",
+        },
+        {
+            id: 3,
+            title: "When keyboarding, all of the following are examples of good posture, EXCEPT:",
+            answers: ["Eyes on Monitor", "Fingers on Home Row", "Back Straight", "Elbows On The table"],
+            correct: "Elbows On The table",
+        },
+        {
+            id: 4,
+            title: "RAM stands for:",
+            answers: ["Random Access Memory", "Rugs And Mud", "Raspbery and Mayonnaise", "Really Awesome Monkeys"],
+            correct: "Random Access Memory",
+        },
+        {
+            id: 5,
+            title: "When you save a file, it is permanently saved on the:",
+            answers: ["CPU", "Monitor", "Keyboard", "Hard Drive"],
+            correct: "Hard Drive",
+        },
+    ]
+}
